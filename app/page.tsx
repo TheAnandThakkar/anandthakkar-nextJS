11c7a7d7d87521299631cc003e4b7f63aa75f5f9
Anand Thakkar <anand.thakkar92@gmail.com>
2025-08-26 18:47:25 +0530

    changes in the website

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
       </h1>
       <p className="mb-4">
         {`Backend Developer & AWS Certified Cloud Practitioner with 4+ years in fintech/SaaS. Skilled in Java, Spring Boot, AWS, scalable APIs, and cloud
-optimisation. Strong cross-functional experience bridging engineering and product.`}
+          optimisation. Strong cross-functional experience bridging engineering and product.`}
       </p>
       <div className="my-8">
         <BlogPosts />
