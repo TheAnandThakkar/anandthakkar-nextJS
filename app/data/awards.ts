e10bdb307ce0d7d1867dc4177634ec1064b826d2
Anand Thakkar <anand.thakkar92@gmail.com>
2025-08-29 09:54:27 +0530

    google developer url has been added

--- app/data/awards.ts
+++ app/data/awards.ts
@@ -7,4 +7,11 @@ export const awards = [
     description:
       "Credential that validates foundational knowledge of AWS Cloud concepts, services, security, pricing, and support. Demonstrates the ability to effectively communicate cloud value to both technical and non-technical stakeholders.",
   },
+  {
+    title: "Google Developer Badges",
+    url: "https://g.dev/TheAnandThakkar",
+    icon: "/google-dev.png",
+    description:
+      "Recognizes expertise and contributions in building applications using Google technologies. Validates proficiency in Google APIs, web and mobile development, and best practices for scalable and performant solutions.",
+  },
 ];
